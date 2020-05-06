9ebdfe6830df51d9a74a4f32facd85858f8842b6
unknown <zhh102000@gmail.com>
2020-05-06 18:00:55 +0900

    check

--- intregral_image.cpp
+++ intregral_image.cpp
@@ -1,6 +1,5 @@
 #include <cstdlib>
 #include <iostream>
-#include <time.h>
 
 #define WIDTH 640
 #define HEIGHT 320
@@ -78,31 +77,5 @@ int main()
     if(sum1 != sum2) cout<<"error\n";
 	delete[] image;
 	delete[] ii_image;
-
-
-    Time t1
-    integral_image(image, WIDTH, HEIGHT, ii_image);
-    time t2
-    int n=1 or 10 or 100 or 1000;
-    int left, top;
-    int sum1=0, sum2=0;
-    int time1=t2-t1; time2=0;
-    for(int i=0; i<n; i++) {
-	    left = rand()%(WIDTH-20);
-		top = rand()%(HEIGHT-20);
-		time t3
-		sum1 += integral_image_value(ii_image, WIDTH, HEIGHT, left, top, left+10, top+10)
-		time t4
-		time t5
-		sum2 += image_sum_value(image, WIDTH, HEIGHT, left, top, left+10, top+10);
-		time t6;
-		time1+=(t4-t3); time2+=(t6-t5);
-}
-printf(“sum1: %d, time1: %d\n”, sum1, time1);
-printf(“sum2: %d, time2: %d\n”, sum2, time2);
-
-
-
-
 
 }
