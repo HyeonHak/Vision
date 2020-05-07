087d3ed490650638b38d8592af57732cf5362b88
HyeonHak <52483468+HyeonHak@users.noreply.github.com>
2020-05-07 18:44:39 +0900

    first end

--- Hungarian algorithm.cpp
+++ Hungarian algorithm.cpp
@@ -2,60 +2,6 @@
 #include <float.h>
 using namespace std;
 
-int Mat_check(float **Mat, const int N, const int M)
-{
-    int *arr = new int[N];
-    int *arr_col = new int[M];
-    int flag;
-
-    for (int i = 0; i < N; i++)
-        arr[i] = 0;
-    for (int i = 0; i < N; i++)
-    {
-        flag = 0;
-        for (int j = 0; j < M; j++)
-        {
-            if (Mat[i][j] == 0 && arr[j] == 0)
-            {
-                flag = 1;
-                arr[j] = 1;
-                break;
-            }
-        }
-        if (flag == 0)
-        {
-            delete[] arr;
-            delete[] arr_col;
-            return (false);
-        }
-    }
-
-    for (int i = 0; i < M; i++)
-        arr_col[i] = 0;
-    for (int i = 0; i < M; i++)
-    {
-        flag = 0;
-        for (int j = 0; j < N; j++)
-        {
-            if (Mat[i][j] == 0 && arr_col[j] == 0)
-            {
-                flag = 1;
-                arr_col[i] = 1;
-                break;
-            }
-        }
-        if (flag == 0)
-        {
-            delete[] arr;
-            delete[] arr_col;
-            return (false);
-        }
-    }
-    delete[] arr;
-    delete[] arr_col;
-    return (true);
-}
-
 void make_minimum_mat(float **Cost, float **Mat, const int N, const int M)
 {
     float MIN;
@@ -80,20 +26,48 @@ void make_minimum_mat(float **Cost, float **Mat, const int N, const int M)
             if (Mat[j][i] < MIN)
                 MIN = Mat[j][i];
         }
-        //cout << "MIN: " << MIN << endl;
         for (int j = 0; j < N; j++)
             Mat[j][i] = Mat[j][i] - MIN;
     }
 }
 
-float calc_mat(float **Mat, const int N, const int M, const int MODE, float *assignment_index)
+void make_maximum_mat(float **Cost, float **Mat, const int N, const int M)
 {
-    float ret = 0;
-    int *arr = new int[N];
+    float MAX;
+    float MIN;
+
     for (int i = 0; i < N; i++)
-        arr[i] = 0;
-    delete[] arr;
-    return (ret);
+    {
+        MAX = FLT_MIN;
+        for (int j = 0; j < M; j++)
+        {
+            if (Cost[i][j] > MAX)
+                MAX = Cost[i][j];
+        }
+        for (int j = 0; j < M; j++)
+            Mat[i][j] = MAX - Cost[i][j];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << Mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < M; i++)
+    {
+        MIN = FLT_MAX;
+        for (int j = 0; j < N; j++)
+        {
+            if (Mat[j][i] < MIN)
+                MIN = Mat[j][i];
+        }
+        for (int j = 0; j < N; j++)
+            Mat[j][i] = Mat[j][i] - MIN;
+    }
 }
 
 int Step3_Mark(int **check, float **Mat, bool *col, bool *row, int N, int M, int zero_cnt)
@@ -133,21 +107,241 @@ int Step3_Mark(int **check, float **Mat, bool *col, bool *row, int N, int M, int
         }
     }
     return (ret);
+}
+
+int is_vaild(int N, float **Mat, float *assignment_index, int MODE)
+{
+    int flag = 1;
+    int cnt;
+    int j;
+    int ans = 0;
+    int **check = new int *[N];
+
+    for (int i = 0; i < N; i++)
+        check[i] = new int[N];
     for (int i = 0; i < N; i++)
     {
-        if (row[i])
+        for (int j = 0; j < N; j++)
+            check[i][j] = 0;
+    }
+
+    while (flag)
+    {
+        flag = 0;
+        int j_idx;
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < M; j++)
+            cnt = 0;
+            for (j = 0; j < N; j++)
             {
-                if (!col[j] && Mat[i][j] == 0)
-                    return (1);
+                if (Mat[i][j] == 0 && check[i][j] == 0)
+                {
+                    cnt++;
+                    j_idx = j;
+                    if (cnt > 1)
+                        break;
+                }
+            }
+            if (cnt > 1)
+                continue;
+            if (cnt == 1)
+            {
+                flag = 1;
+                ans++;
+                check[i][j_idx] = 1;
+                for (int i = 0; i < N; i++)
+                {
+                    if (Mat[i][j_idx] == 0)
+                        check[i][j_idx] = -1;
+                }
+                for (int k = 0; k < N; k++)
+                {
+                    if (Mat[i][k] == 0)
+                        check[i][k] = -1;
+                }
+                check[i][j_idx] = 1;
             }
         }
     }
-    return (0);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << check[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "COL\n";
+    flag = 1;
+    while (flag)
+    {
+        flag = 0;
+        int i_idx;
+        for (int i = 0; i < N; i++)
+        {
+            cnt = 0;
+            for (j = 0; j < N; j++)
+            {
+                if (Mat[j][i] == 0 && check[j][i] == 0)
+                {
+                    cnt++;
+                    i_idx = j;
+                    if (cnt > 1)
+                        break;
+                }
+            }
+            if (cnt > 1)
+                continue;
+            if (cnt == 1)
+            {
+                flag = 1;
+                ans++;
+                check[i_idx][i] = 1;
+                for (int i = 0; i < N; i++)
+                {
+                    if (Mat[i_idx][i] == 0)
+                        check[i_idx][i] = -1;
+                }
+                for (int k = 0; k < N; k++)
+                {
+                    if (Mat[k][i] == 0)
+                        check[k][i] = -1;
+                }
+                check[i_idx][i] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << check[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (Mat[i][j] == 0 && check[i][j] == 0)
+            {
+
+                for (int k = 0; k < N; k++)
+                {
+                    if (Mat[i][k] == 0 && check[i][k] == 0)
+                        check[i][k] = -1;
+                    if (Mat[k][j] == 0 && check[k][j] == 0)
+                        check[k][j] = -1;
+                }
+                check[i][j] = 1;
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        cout << check[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << check[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "FUCK : " << ans << endl;
+    int value = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (check[i][j] == 1)
+                assignment_index[value++] = j;
+        }
+    }
+    for (int i = 0; i < N; i++)
+        delete[] check[i];
+    delete[] check;
+
+    if (ans == N)
+        return (1);
+    return (-1);
+
+    /*int ret = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (Mat[i][j] == 0)
+            {
+                if (check[i][j] == 0)
+                {
+                    check[i][j] = 1;
+                    assignment_index[ret] = j;
+                    ret++;
+                    for (int k = 0; k < N; k++)
+                    {
+                        if (Mat[k][j] == 0 && k != i)
+                            check[k][j] = -1;
+                    }
+                    for (int k = j + 1; k < N; k++)
+                    {
+                        if (Mat[i][k] == 0)
+                            check[i][k] = -1;
+                    }
+                    break;
+                }
+                else
+                    check[i][j] = -1;
+            }
+        }
+    }
+    if (ret == N)
+        return (1);
+    return (-1);*/
 }
 
-int Mat_change(float **Mat, int N, int M, float *assignment_index)
+void check_set(int N, float **Mat, int **check)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (Mat[i][j] == 0)
+            {
+                if (check[i][j] == 0)
+                {
+                    check[i][j] = 1;
+                    for (int k = 0; k < N; k++)
+                    {
+                        if (Mat[k][j] == 0 && k != i)
+                            check[k][j] = -1;
+                    }
+                    for (int k = j + 1; k < N; k++)
+                    {
+                        if (Mat[i][k] == 0)
+                            check[i][k] = -1;
+                    }
+                    break;
+                }
+                else
+                    check[i][j] = -1;
+            }
+        }
+    }
+}
+
+int Mat_change(float **Mat, int N, int M, float *assignment_index, int MODE)
 {
     //check 배열 2차원..?
     float MIN = FLT_MAX;
@@ -156,7 +350,7 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
     bool *col = new bool[M];
     int zero_cnt = 0;
 
-    //init
+    //init check, row, col
     for (int i = 0; i < N; i++)
         check[i] = new int[M];
     for (int i = 0; i < N; i++)
@@ -164,8 +358,6 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
         row[i] = false;
         for (int j = 0; j < M; j++)
         {
-            if (Mat[i][j] == 0)
-                zero_cnt++;
             check[i][j] = 0;
             col[j] = false;
         }
@@ -173,6 +365,14 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
 
     int ret = 0;
     //check init (step 3 assign)
+    cout << "START\n";
+
+    if (is_vaild(N, Mat, assignment_index, MODE) == 1)
+        return (1);
+    //    return (1);
+    cout << "FUCK\n";
+    check_set(N, Mat, check);
+    /*
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
@@ -203,15 +403,7 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
     }
     if (ret == N)
         return (1);
-    /*for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cout << check[i][j] << " ";
-        }
-        cout << endl;
-    }*/
-
+*/
     //Mark all rows having no assignments
     for (int i = 0; i < N; i++)
     {
@@ -228,36 +420,11 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
         if (!flag)
             row[i] = true;
     }
-    /*cout << "INIT\n";
-    for (int i = 0; i < N; i++)
-        cout << row[i] << " ";
-    cout << endl;
-    for (int j = 0; j < M; j++)
-        cout << col[j] << " ";
-    cout << endl;
-    cout << endl;*/
-    //step 3 (Mark)
+
     while (Step3_Mark(check, Mat, col, row, N, M, zero_cnt))
     {
-        /*for (int i = 0; i < N; i++)
-            cout << row[i] << " ";
-        cout << endl;
-        for (int j = 0; j < M; j++)
-            cout << col[j] << " ";
-        cout << endl;
-        cout << endl;*/
     }
 
-    /*cout << "ROW\n";
-    for (int i = 0; i < N; i++)
-        cout << row[i] << " ";
-    cout << endl;
-    for (int i = 0; i < M; i++)
-        cout << col[i] << " ";
-    cout << endl;
-
-    cout << "START\n";*/
-    //row = false && col = true
     float MIN_VALUE = FLT_MAX;
     for (int i = 0; i < N; i++)
     {
@@ -273,7 +440,7 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
             }
         }
     }
-    //cout << MIN_VALUE << endl;
+
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
@@ -294,29 +461,49 @@ int Mat_change(float **Mat, int N, int M, float *assignment_index)
         }
     }
 
+    cout << endl;
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            check[i][j] = 0;
             cout << Mat[i][j] << " ";
         }
         cout << endl;
     }
-    cout << endl;
+    cout << "BYE";
     return (0);
 }
 
 float Solve(float **Cost, const int N, const int M, const int MODE, float *assignment_index)
 {
     float ret;
-    int min_row, min_col;
+    float total = 0;
     float **Mat = new float *[N];
 
     for (int i = 0; i < M; i++)
         Mat[i] = new float[M];
 
-    make_minimum_mat(Cost, Mat, N, M);
+    if (MODE == 0)
+        make_minimum_mat(Cost, Mat, N, M);
+    else if (MODE == 1)
+        make_maximum_mat(Cost, Mat, N, M);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << Mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    int ret2 = 0;
+    while (Mat_change(Mat, N, M, assignment_index, MODE) == 0)
+    {
+        if (ret2 == 3)
+            break;
+        ret2++;
+    }
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
@@ -326,33 +513,6 @@ float Solve(float **Cost, const int N, const int M, const int MODE, float *assig
         cout << endl;
     }
 
-    //while (!Mat_check(Mat, N, M))
-    //{
-    while (Mat_change(Mat, N, M, assignment_index) == 0)
-    {
-    }
-    /*return (ret);
-        cout << "fail";
-        break;*/
-    //}
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-            cout << Mat[i][j] << " ";
-        cout << endl;
-    }
-    float total = 0;
-    for (int i = 0; i < N; i++)
-    {
-        total += Cost[i][(int)assignment_index[i]];
-        cout << "COST" << Cost[i][(int)assignment_index[i]] << endl;
-        cout << assignment_index[i] << " ";
-    }
-
-    cout << "\n"
-         << total << "\n";
-    return (ret);
-    ret = calc_mat(Mat, N, M, MODE, assignment_index);
     for (int i = 0; i < N; i++)
         delete[] Mat[i];
     delete[] Mat;
@@ -363,26 +523,66 @@ int main(void)
 {
     int N, M, MODE;
     float input;
+    float **Cost;
+    float *assignment_index;
+    float total = 0;
+    int len = 0;
 
     cout << " Input row, column : ";
     cin >> N >> M;
-
-    float **Cost = new float *[N];
-    float *assignment_index = new float[N];
-
-    for (int i = 0; i < N; i++)
-        Cost[i] = new float[M];
+    len = N;
+    if (N > M)
+    {
+        Cost = new float *[N];
+        for (int i = 0; i < N; i++)
+            Cost[i] = new float[N];
+    }
+    else
+    {
+        Cost = new float *[M];
+        for (int i = 0; i < M; i++)
+            Cost[i] = new float[M];
+    }
+    assignment_index = new float[N];
 
     cout << " Input Matrix : \n";
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
             cin >> Cost[i][j];
-    cout << " Input Mode (1. Minimize, 2. Maximize): ";
+    if (N < M)
+    {
+        for (int i = N; i < M; i++)
+        {
+            for (int j = 0; j < M; j++)
+                Cost[i][j] = 10;
+        }
+        N = M;
+    }
+    else
+    {
+        for (int i = M; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Cost[j][i] = 10;
+            }
+        }
+        M = N;
+    }
+
+    cout << " Input Mode (0. Minimize, 1. Maximize): ";
     cin >> MODE;
     Solve(Cost, N, M, MODE, assignment_index);
 
-    for (int i = 0; i < N; i++)
+    cout << "assignment index : ";
+    for (int i = 0; i < len; i++)
+    {
+        total += Cost[i][(int)assignment_index[i]];
         cout << assignment_index[i] << " ";
+    }
+
+    cout << "\n";
+    cout << "Total cost : " << total;
     for (int i = 0; i < N; i++)
         delete[] Cost[i];
     delete[] Cost;
